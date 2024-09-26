43151a792ea598c1b2e1cbe81b6bd4f8718f71bf
Miya096jp <miyawaki.yske@gmail.com>
2024-09-26 14:05:14 +0900

    Add published_at for draft, published, and scheduled blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,13 +3,10 @@ before_action :authenticate_user!, except: [:index, :show]
 before_action :set_blog_post, except: [:index, :new, :create]
 
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
   end
 
   def new
@@ -26,11 +23,9 @@ before_action :set_blog_post, except: [:index, :new, :create]
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -39,7 +34,6 @@ before_action :set_blog_post, except: [:index, :new, :create]
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
@@ -47,11 +41,11 @@ before_action :set_blog_post, except: [:index, :new, :create]
 private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
